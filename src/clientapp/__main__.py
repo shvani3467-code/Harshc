c39e7eaa7c9d4fbbc9602313f59839415775407a
Harsh <sh.vani3467@gmail.com>
2026-07-23 10:53:46 +0530

    Refactor main entry point to use clientapp.main
    Refactor the main entry point to use the main function from clientapp.app.

--- src/clientapp/__main__.py
+++ src/clientapp/__main__.py
@@ -1,25 +1,5 @@
-import toga
-from toga.style import Pack
-from toga.style.expression import COLUMN
+from clientapp.app import main
 
-
-class ClientApp(toga.App):
-
-    def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN, padding=20))
-
-        welcome_label = toga.Label(
-            "App Working Successfully! 🎉",
-            style=Pack(padding=(0, 0, 15, 0)),
-        )
-
-        main_box.add(welcome_label)
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
-
-
-def main():
-    return ClientApp("Client App", "com.example.clientapp")
+if __name__ == '__main__':
+    main()
 
