9a0e6cb81380035265515fa6e4ac644fab438fa2
Harsh <sh.vani3467@gmail.com>
2026-07-23 10:36:48 +0530

    Correct funding model platforms text

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello Harsh, app is running!")
+import toga
+from toga.style import Pack
+from toga.style.expression import COLUMN, ROW
+
+
+def build(app):
+    box = toga.Box(style=Pack(direction=COLUMN, padding=20))
+    label = toga.Label(
+        'Hello Harsh! App Successfully Running!',
+        style=Pack(padding=(0, 0, 10, 0))
+    )
+    box.add(label)
+    return box
+
+
+def main():
+    return toga.App('Harsh App', 'org.example.harshc', startup=build)
+
+
+if __name__ == '__main__':
+    main()
+
